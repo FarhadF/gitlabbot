4f33d698dafb0424c6112729a23a462d4f4a391d
FarhadF <farhad.farahi@gmail.com>
2017-08-27 17:47:06 +0430

    processing the hook for merge requests

--- main.go
+++ main.go
@@ -31,6 +31,7 @@ type hook struct {
 		AuthorId        int    `json:"author_id"`
 		State           string `json:"state"`
 		MergeStatus     string `json:"merge_status"`
+		Iid             int    `json:"iid"`
 	} `json:"object_attributes"`
 	MergeRequest struct {
 		Iid int `json:"iid"`
@@ -48,7 +49,9 @@ func main() {
 }
 
 func Handle(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-
+	/*	read, _ := ioutil.ReadAll(r.Body)
+		fmt.Println(string(read))
+	*/
 	var h hook
 	err := json.NewDecoder(r.Body).Decode(&h)
 
@@ -59,12 +62,13 @@ func Handle(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Println(h.ObjectKind)
 	fmt.Println(h.ObjectAttributes.Id)
 	fmt.Println(h)
-
 	id, err := CheckStatus(h)
-	if err != nil {
+	if err != nil && err.Error() == "sql: no rows in result set" {
+		fmt.Println("No ROws!!")
+	} else if err != nil {
 		panic(err)
 	}
-	fmt.Prinln("working id:", id)
+	fmt.Println("working id:", id)
 
 	/*
 		read, _ := ioutil.ReadAll(r.Body)
@@ -76,11 +80,12 @@ func InitDb() {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
 		"password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
-	Db, err := sql.Open("postgres", psqlInfo)
+	var err error
+	Db, err = sql.Open("postgres", psqlInfo)
 	if err != nil {
 		panic(err)
 	}
-	defer Db.Close()
+	//defer Db.Close()
 	err = Db.Ping()
 	if err != nil {
 		fmt.Println("Connection to database Failed:", err)
@@ -90,8 +95,10 @@ func InitDb() {
 
 }
 
+//Check the status of Merge Request, Im Processing Open and Not Merged only.
 func CheckStatus(h hook) (int, error) {
-	Db.QueryRow("SELECT m.id FROM merge_requests AS m WHERE m.target_project_id = $1 AND m.iid = $2 AND m.state != 'closed' AND m.state != 'merged'", h.ObjectAttributes.TargetProjectId, h.MergeRequest.Iid)
+	fmt.Println("beforequery:", h.ObjectAttributes.TargetProjectId, h.ObjectAttributes.Iid)
+	row := Db.QueryRow(`SELECT id FROM merge_requests WHERE target_project_id = $1 AND iid = $2 AND state != 'closed' AND state != 'merged'`, h.ObjectAttributes.TargetProjectId, h.ObjectAttributes.Iid)
 	var id int
 	err := row.Scan(&id)
 	if err != nil {
